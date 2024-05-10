ac0b8c697d99d1b738be0aedaea2f3b05574d3c9
Ted Poatsy <ted@crafted.solutions>
2024-05-10 11:41:29 -0400

    Initial commit

---
+++ src/Board.js
@@ -0,0 +1,22 @@
+
+export default function Board() {
+    return (
+        <>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+        </>
+    );
+}
